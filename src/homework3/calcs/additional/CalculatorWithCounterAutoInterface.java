1ace10dc6195c1e85c69207bdc8c0964a60bd61c
Maryia Hil <hilmerry@gmail.com>
2021-01-26 21:54:48 +0300

    a small fix for CalculatorWithCounterAutoInterface

--- src/homework3/calcs/additional/CalculatorWithCounterAutoInterface.java
+++ src/homework3/calcs/additional/CalculatorWithCounterAutoInterface.java
@@ -2,7 +2,7 @@ package homework3.calcs.additional;
 
 import homework3.calcs.api.ICalculator;
 
-public class CalculatorWithCounterAutoInterface implements ICalculator {
+public class CalculatorWithCounterAutoInterface {
     private long count;
 
     private final ICalculator calculator;
@@ -11,43 +11,37 @@ public class CalculatorWithCounterAutoInterface implements ICalculator {
         this.calculator = calculator;
     }
 
-    @Override
+
     public double divide(double a, double b) {
         count++;
         return calculator.divide(a, b);
     }
 
-    @Override
     public double multiply(double a, double b) {
         count++;
         return calculator.multiply(a, b);
     }
 
-    @Override
     public double minus(double a, double b) {
         count++;
         return calculator.minus(a, b);
     }
 
-    @Override
     public double plus(double a, double b) {
         count++;
         return calculator.plus(a, b);
     }
 
-    @Override
     public double pow(double value, int powValue) {
         count++;
         return calculator.pow(value, powValue);
     }
 
-    @Override
     public double abs(double a) {
         count++;
         return calculator.abs(a);
     }
 
-    @Override
     public double sqrt(double a) {
         count++;
         return calculator.sqrt(a);
